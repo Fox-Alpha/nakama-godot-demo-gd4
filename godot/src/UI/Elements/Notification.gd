125c87f1c33dcfdc8109705130827c05640d0d6a
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-04-12 15:12:09 +0200

    Fixes
    remove Tween Node
    Fix create Tween
    Fix await
    Fix colors
    fix property names

--- godot/src/UI/Elements/Notification.gd
+++ godot/src/UI/Elements/Notification.gd
@@ -6,16 +6,16 @@ const DURATION_SCALE := 0.25
 const DURATION_VISIBLE := 2.0
 const DURATION_FADE_OUT := 2.0
 
-onready var label: RichTextLabel = $RichTextLabel
-onready var tween: Tween = $Tween
+@onready var label: RichTextLabel = $RichTextLabel
+@onready var tween: Tween = create_tween()
 
 
 func _ready() -> void:
-	rect_pivot_offset = rect_size
-	rect_scale = Vector2.ZERO
+	pivot_offset = size
+	scale = Vector2.ZERO
 
 
-func setup(username: String, color := Color.white, disconnected := false) -> void:
+func setup(username: String, color := Color.WHITE, disconnected := false) -> void:
 	label.bbcode_text = (
 		"[color=#%s]%s[/color] %s"
 		% [color.to_html(false), username, "left" if disconnected else "joined"]
@@ -34,8 +34,8 @@ func setup(username: String, color := Color.white, disconnected := false) -> voi
 	tween.interpolate_property(
 		self,
 		"modulate",
-		Color.white,
-		Color.transparent,
+		Color.WHITE,
+		Color.TRANSPARENT,
 		DURATION_FADE_OUT,
 		Tween.TRANS_LINEAR,
 		Tween.EASE_IN_OUT,
@@ -43,5 +43,5 @@ func setup(username: String, color := Color.white, disconnected := false) -> voi
 	)
 # warning-ignore:return_value_discarded
 	tween.start()
-	yield(tween, "tween_all_completed")
+	await tween.tween_all_completed
 	queue_free()
