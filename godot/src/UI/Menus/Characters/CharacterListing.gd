b6f609eb3717dc89e5415d9483e571f97f253053
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-04-12 15:38:39 +0200

    fixes
    getter / setter,
    annotation
    get_position_in_parent, removed from function, now get_index()
    await

--- godot/src/UI/Menus/Characters/CharacterListing.gd
+++ godot/src/UI/Menus/Characters/CharacterListing.gd
@@ -6,19 +6,19 @@ signal requested_deletion(character_index)
 signal character_selected(index)
 signal character_accepted(index)
 
-var is_enabled := true setget set_is_enabled
+var is_enabled := true : set = set_is_enabled
 
-onready var texture := $HBoxContainer/TextureRect
-onready var label := $HBoxContainer/Label
-onready var delete_button := $HBoxContainer/DeleteButton
+@onready var texture := $HBoxContainer/TextureRect
+@onready var label := $HBoxContainer/Label
+@onready var delete_button := $HBoxContainer/DeleteButton
 
 
 func _gui_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_accept"):
-		emit_signal("character_accepted", get_position_in_parent())
+		emit_signal("character_accepted", get_index())
 		accept_event()
 	if event is InputEventMouseButton and event.doubleclick:
-		emit_signal("character_accepted", get_position_in_parent())
+		emit_signal("character_accepted", get_index()) #get_position_in_parent())
 		accept_event()
 
 
@@ -30,14 +30,14 @@ func setup(character_name: String, character_color: Color) -> void:
 func set_is_enabled(value: bool) -> void:
 	is_enabled = value
 	if not delete_button:
-		yield(self, "ready")
+		await self.ready
 	disabled = not is_enabled
 	delete_button.disabled = not is_enabled
 
 
 func _on_DeleteButton_pressed() -> void:
-	emit_signal("requested_deletion", get_position_in_parent())
+	emit_signal("requested_deletion", get_index())
 
 
 func _on_focus_entered() -> void:
-	emit_signal("character_selected", get_position_in_parent())
+	emit_signal("character_selected", get_index())
